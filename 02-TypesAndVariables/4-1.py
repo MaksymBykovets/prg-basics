13feb71cf7a4aa34f6d3eacf1f9d55e5a8831ca4
Maksym Bykovets <236354@student.uek.krakow.pl>
2024-10-11 10:18:26 +0200

    section 4

---
+++ 02-TypesAndVariables/4-1.py
@@ -0,0 +1,10 @@
+###
+# Printing student's personal data
+#
+age = 22
+heigth = 189
+after_6_years = age + 6
+name = "Adam"
+print(f"My name is {name}.")
+print(f"I am {age} years old, and my height is {heigth} cm.")
+print(f'In 6 years I will be {after_6_years} years old.'  )
