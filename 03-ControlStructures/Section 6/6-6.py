648f4f92cd13c40d7e6c8d7003f7e5bb76a640e3
Maksym Bykovets <236354@student.uek.krakow.pl>
2024-10-24 19:45:03 +0200

    few tasks from the 6 Section

---
+++ 03-ControlStructures/Section 6/6-6.py
@@ -0,0 +1,12 @@
+##
+# Write a program that asks for the number of hours of parking, 
+# then calculates and prints the correct fee.
+#
+# 1-2 hours: 5 PLN
+# 3-6 hours: 15 PLN
+# Over 6 hours: 20 PLN
+##
+
+print("Welcome to Parking Meter")
+
+hours = int(input("Enter the number of parking hours "))
