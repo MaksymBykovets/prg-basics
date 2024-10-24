710e5c4a872069dfce4d003773d419989fb11ad6
Maksym Bykovets <236354@student.uek.krakow.pl>
2024-10-24 17:49:54 +0200

    Section 5 done

--- 03-ControlStructures/Section 5/5-8(atm).py
+++ 03-ControlStructures/Section 5/5-8(atm).py
@@ -10,9 +10,11 @@ while True:
     print("1. Check balance")
     print("2. Deposit")
     print("3. Withdraw")
-    print("4. Exit")
+    print("4. Check PIN")
+    print("5. Change PIN")
+    print("6. Exit")
 
-    choice = input("Choose an option (1-4): ")
+    choice = input("Choose an option (1-6): ")
     print()
 
     if choice == '1':
@@ -28,7 +30,16 @@ while True:
             print(f"€{amount} has been withdrawn. New balance: €{balance}")
         else:
             print("Insufficient balance.")
-    elif choice == '4':
+    elif choice == "4" : 
+        print(f"Your current PIN is : {pin}")
+    elif choice == "5" : 
+        new_pin = (input("Enter new PIN (4 char): ") )
+        if new_pin.isdigit() and len(new_pin) == 4  :
+            pin = new_pin
+            print(f"New PIN is : {new_pin}")
+        else: 
+            print(f"You entered incorrect type of PIN! Your PIN still : {pin}") 
+    elif choice == '6':
         print("Exiting... Thank you for using the ATM!")
         break  # Exit the loop
     else:
